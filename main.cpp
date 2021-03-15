327002ed8b85345072a7f0831ed6e82396f9ca44
Elizaveta <eliza_bazanova@mail.ru>
2021-03-15 11:00:01 +0300

    добавлены комментарии

--- main.cpp
+++ main.cpp
@@ -46,35 +46,41 @@ int sum(int* array, int y){
 
 // Подсчитывание эллементов для всего массива массивов
 
-int summa(int** array, int x, int y, int first, int& s){
+int summa(int** array, int x, int y, int first, int& s, int num){
     s = 0;
-    for(int i = first; i < x; i += 4){
+    for(int i = first; i < x; i += num){
         s += sum(array[i], y);
     } 
 }
 
 int main(){
+    // Подсчитывание кол-ва доступных потоков, поддерживаемых процессором
     int num;
     num = std::thread::hardware_concurrency();
     std::cout << num << std::endl;
 
+    // Создание вектора и вывод его значений в консоль
     int x = 100;
     int y = 100;
     int** array = create(x, y);
     printInfo(array, x, y);   
 
+    // Создание вектора потоков и вектора переменных int, куда сохраняется сумма строк вектора array
     std::vector <std::thread> threadVec;
     std::vector <int> sumVec(num);
 
+    // Вызов потоков, для подсчета суммы строк вектора array
     for(int i = 0; i < num; ++i){
-        std::thread thr([&array, x, y, &sumVec, i](){summa(array, x, y, i, sumVec[i]);});
+        std::thread thr([&array, x, y, &sumVec, i, num](){summa(array, x, y, i, sumVec[i], num);});
         threadVec.push_back(std::move(thr));        
     }
 
+    // Добавление потоков в очередь ожидания завершения подсчета последнего потока
     for(auto& t : threadVec){
         t.join();
     }
 
+    // Подсчет общей суммы из всех строк вектора 
     int fSumma = 0;
     for(int i = 0; i < num; ++i){
         fSumma += sumVec[i];
@@ -82,10 +88,12 @@ int main(){
 
     std::cout << "Сумма элементов матрицы равна: " << fSumma << std::endl;
 
+    // Вывод в консоль суммы строки вектора
     for(int i = 0; i < num; ++i){
         std::cout << sumVec[i] << std::endl;
     }
     
+    // Удаление вектора
     remove(array, x, y);
 
     return 0;
